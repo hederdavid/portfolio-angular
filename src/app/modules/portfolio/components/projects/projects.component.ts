0b527e47f03ee82c4da5f5b6b56c9b0d21c372eb
Heder David <hedermd6@gmail.com>
2024-06-29 17:22:13 -0300

    finalizado

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog)
 
   public arrayProjects = signal<IProjects[]>([
     {
@@ -24,7 +29,84 @@ export class ProjectsComponent {
           href: 'https://vidafullstack.com.br'
         }
       ]
+    },
+    {
+      src: '/assets/img/projects/vfull.png',
+      alt: 'Projeto vida full stack',
+      tittle: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: 'Projeto desenvolvido por Denner Troquatte',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br'
+        }
+      ]
+    },
+    {
+      src: '/assets/img/projects/vfull.png',
+      alt: 'Projeto vida full stack',
+      tittle: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: 'Projeto desenvolvido por Denner Troquatte',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br'
+        }
+      ]
+    },
+    {
+      src: '/assets/img/projects/vfull.png',
+      alt: 'Projeto vida full stack',
+      tittle: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: 'Projeto desenvolvido por Denner Troquatte',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br'
+        }
+      ]
+    },
+    {
+      src: '/assets/img/projects/vfull.png',
+      alt: 'Projeto vida full stack',
+      tittle: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: 'Projeto desenvolvido por Denner Troquatte',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br'
+        }
+      ]
+    },
+    {
+      src: '/assets/img/projects/vfull.png',
+      alt: 'Projeto vida full stack',
+      tittle: 'Vida Full Stack',
+      width: '100px',
+      height: '51px',
+      description: 'Projeto desenvolvido por Denner Troquatte',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br'
+        }
+      ]
     }
   ])
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 
 }
